43faeb269c8ff3cb5fe3be1644fd466803e8b6ce
Dale Stammen <dalestam@microsoft.com>
2016-01-25 15:48:23 -0800

    start of winrt support

---
+++ tr_winrt_tcp/MainPage.xaml.cpp
@@ -0,0 +1,37 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+#include "MainPage.xaml.h"
+#include "test-tr_tcp.h"
+
+
+using namespace tr_winrt_tcp;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::System::Threading;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+    auto dispatcher = Windows::UI::Xaml::Window::Current->CoreWindow->Dispatcher;
+    auto workItemHandler = ref new Windows::System::Threading::WorkItemHandler([this, dispatcher](Windows::Foundation::IAsyncAction ^ action)
+    {
+        dotest();
+    });
+    Windows::System::Threading::ThreadPool::RunAsync(workItemHandler, Windows::System::Threading::WorkItemPriority::High, Windows::System::Threading::WorkItemOptions::TimeSliced);
+}
