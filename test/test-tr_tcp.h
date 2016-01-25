6ccb89b2df2d4ccb0f480a78197a3794dafc8373
Dale Stammen <dalestam@microsoft.com>
2016-01-25 15:49:33 -0800

    start of winrt support

---
+++ test/test-tr_tcp.h
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2014,2015 NetEase, Inc. and other Pomelo contributors
+ * MIT Licensed.
+ */
+
+#ifndef TEST_TR_TCP_H
+#define TEST_TR_TCP_H
+
+#ifdef WINRT
+int dotest();
+#endif
+
+#endif // TEST_TR_TCP_H
